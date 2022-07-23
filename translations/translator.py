60621d1e94341bc329c6b77a4890932cef453499
proxpxd <piotr10tutek@poczta.onet.pl>
2022-07-23 19:27:15 +0200

    add not requiering to_lang in translator

--- translations/translator.py
+++ translations/translator.py
@@ -6,7 +6,7 @@ from web.connector import Connector
 
 class Translator:
 
-    def __init__(self, from_lang: str, to_lang: str, word: str = None):
+    def __init__(self, from_lang: str, to_lang: str = None, word: str = None):
         self._connector: Connector = Connector(from_lang, to_lang, word)
         self._parser: Parser = Parser()
         self._html: str = ""

ce939563723d23e81e8c4944a267e7b09965a696
proxpxd <piotr10tutek@poczta.onet.pl>
2022-07-24 12:33:12 +0200

    add return translation out of the parser

--- translating/parsing/parser.py
+++ translating/parsing/parser.py
@@ -1,14 +1,27 @@
-from bs4.element import Tag
+from dataclasses import dataclass
+
 from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+
+@dataclass
+class TranslationParts:
+    TRANSLATION: str = 'translation'
+    GENDER: str = 'gender'
+    PART_OF_SPEECH: str = 'part of speech'
 
 
 class Parser:
 
     def __init__(self, text=""):
-        self._soup = BeautifulSoup(text, features="html.parser")
+        self._soup = None
+        self._init_soup(text)
 
     def set_page_text(self, text: str):
-        self._text = text
+        self._init_soup(text)
+
+    def _init_soup(self, text: str):
+        self._soup = BeautifulSoup(text, features="html.parser")
 
     def parse(self):
         translations = []
@@ -21,6 +34,11 @@ class Parser:
         spans = self._get_spans(translation_tag)
         part_of_speech = self._get_part_of_speech(spans)
         gender = self._get_gender(spans)
+        return {
+            TranslationParts.TRANSLATION: translation,
+            TranslationParts.PART_OF_SPEECH: part_of_speech,
+            TranslationParts.GENDER: gender
+        }
 
     def _get_translation(self, translation_tag: Tag) -> str:
         return translation_tag.select_one('span[data-element="phrase"]').text[1:-1]  # TODO check if and why the try except block is needed

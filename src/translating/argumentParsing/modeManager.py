5ec67fe4dc6b8ebc353fccf49ca6874ef20b5599
proxpxd <piotr10tutek@poczta.onet.pl>
2022-08-06 13:05:03 +0200

    refactor mode managers filter method

--- src/translating/argumentParsing/modeManager.py
+++ src/translating/argumentParsing/modeManager.py
@@ -96,36 +96,43 @@ class ModesManager:
     def get_config_args(self, config: str) -> list[str]:
         return self._modes[config] if config in self._modes else []
 
-    def get_max_arity(self, mode: str) -> int:
-        return functools.reduce(lambda m1, m2: m1 if m1 > m2 else m2,
-                                (_modes_to_arity_map[modes] for modes in _modes_to_arity_map if mode in modes))
-
-    def filter_modes_out_of_args(self, args: list[str]) -> list[str]:  # TODO: look for a clean-code refactor
+    def filter_modes_out_of_args(self, args: list[str]) -> list[str]:
         i = 0
         while 0 <= i < len(args):
-            arg = args[i]
-            if not self._is_mode(arg):
-                i += 1
-                continue
-
-            if arg not in self._modes:
-                if arg in _modes_map:
-                    arg = _modes_map[arg]
-                self._modes[arg] = []
+            i = self._find_index_of_next_arg(i, args)
+            arg = self._get_key_for_arg(args[i])
             del args[i]
+            self._initialize_modes_dictionary_if_needed(arg)
 
-            arity: int = self.get_max_arity(arg)
-            while len(args) < i + arity:
-                arity -= 1
-            last_mode_argument_index: int = i + arity
-
+            last_mode_argument_index = self._get_last_index_of_mode_argument(i, arg, args)
             self._modes[arg].extend(args[i:last_mode_argument_index])
             del args[i:last_mode_argument_index]
             i = last_mode_argument_index
         return args
 
-    def _is_mode(self, arg: str):
-        return arg.startswith('-')
+    def _find_index_of_next_arg(self, i: int, args: list[str]) -> int:
+        while not self._is_mode(args[i]):
+            i += 1
+        return i
+
+    def _get_key_for_arg(self, arg: str) -> str:
+        if arg in _modes_map:
+            arg = _modes_map[arg]
+        return arg
+
+    def _initialize_modes_dictionary_if_needed(self, arg: str):
+        if arg not in self._modes:
+            self._modes[arg] = []
+
+    def get_max_arity(self, mode: str) -> int:
+        return functools.reduce(lambda m1, m2: m1 if m1 > m2 else m2,
+                                (_modes_to_arity_map[modes] for modes in _modes_to_arity_map if mode in modes))
+
+    def _get_last_index_of_mode_argument(self, i: int, arg: str, args: list[str]):
+        arity: int = self.get_max_arity(arg)
+        if len(args) < i + arity:
+            arity = len(args) - i
+        return i + arity
 
     def validate_modes(self) -> list[str]:
         error_messages = []

2586d0dec1d8b4e4aa789d7893a63ea01cc68855
proxpxd <piotr10tutek@poczta.onet.pl>
2022-08-15 13:28:52 +0200

    fix tests names, make more abstract

---
+++ tests/abstractTranslationTest.py
@@ -0,0 +1,57 @@
+import unittest
+
+from src.translating.argumentParsing.configurations import Configurations, Configs
+from src.translating.argumentParsing.intelligentArgumentParser import IntelligentArgumentParser
+from src.translating.constants import TranslationParts
+from src.translating.translator import Translator
+import abc
+
+
+class AbstractTranslationTest(unittest.TestCase, abc.ABC):
+
+    argumentParser: IntelligentArgumentParser = None
+    translator: Translator = Translator()
+    class_sep_size = 20
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        Configurations.init()
+        Configurations.change_conf(Configs.SAVED_LANGS, ['pl', 'en', 'de', 'es', 'uk', 'zh'])
+        Configurations.change_conf(Configs.DEFAULT_TRANSLATIONAL_MODE, cls._get_mode())
+        print('*' * cls.class_sep_size + f' Starting {cls._get_mode()} mode tests! ' + '*' * cls.class_sep_size)
+
+    @classmethod
+    @abc.abstractmethod
+    def _perform_translation(cls):
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def _get_mode(cls) -> str:
+        return None
+
+    def set_input_string(self, input_string: str):
+        AbstractTranslationTest.argumentParser = IntelligentArgumentParser(input_string.split(' '))
+
+    def get_constant_part(self, translation: tuple):
+        return translation[0]
+
+    def get_nth_translation_batch(self, index: int, translation: tuple[str, list]):
+        return translation[1][index]
+
+    def get_nth_translated_word(self, index: int, translation: tuple[str, list]):
+        return translation[1][index][TranslationParts.TRANSLATION]
+
+    def get_word_from_batch(self, batch):
+        return batch[TranslationParts.TRANSLATION]
+
+    def get_gender_from_batch(self, batch):
+        return batch[TranslationParts.GENDER]
+
+    def get_part_of_speech_from_batch(self, batch):
+        return batch[TranslationParts.PART_OF_SPEECH]
+
+    def translate(self):
+        AbstractTranslationTest.argumentParser.parse()
+        translation = self._perform_translation()
+        return list(translation)
---
+++ tests/singleTranslationTest.py
@@ -0,0 +1,69 @@
+from src.translating.argumentParsing.configurations import Configurations
+from src.translating.argumentParsing.constants import FullModes
+from src.translating.web.translatorArgumentException import TranslatorArgumentException
+from tests.abstractTranslationTest import AbstractTranslationTest
+
+
+class SingleTranslationTest(AbstractTranslationTest):
+
+    @classmethod
+    def _get_mode(cls):
+        return FullModes.SINGLE
+
+    def _perform_translation(self):
+        from_lang = self.argumentParser.from_lang
+        to_lang = self.argumentParser.to_langs[0] if self.argumentParser.to_langs else None
+        word = self.argumentParser.words[0] if self.argumentParser.words else None
+        return self.translator.single_translate(word, to_lang, from_lang)
+
+    def test_no_args_set(self):
+        self.set_input_string('t')
+        self.assertRaises(TranslatorArgumentException, self.translate)
+
+    def test_all_args_set(self):
+        tak = 'tak'
+        self.set_input_string(f't {tak} pl zh')
+
+        translations = self.translate()
+        self.assertTrue(len(translations))
+
+        batch = self.get_nth_translation_batch(0, translations[0])
+        translated_word = self.get_word_from_batch(batch)
+
+        self.assertEqual(translations[0][0], tak)
+        self.assertTrue(len(batch))
+        self.assertTrue(translated_word)
+
+    def test_word_and_to_lang_args_set(self):
+        word, from_lang, to_lang = 'ir', 'es', 'pl'
+        Configurations.change_last_used_languages(from_lang)
+        self.set_input_string(f't {word} {to_lang}')
+
+        translation = self.translate()[0]
+        self.assertTrue(len(translation[1]))
+
+        constant_part = self.get_constant_part(translation)
+        batch = self.get_nth_translation_batch(0, translation)
+        translated_word = self.get_word_from_batch(batch)
+        part_of_speech = self.get_part_of_speech_from_batch(batch)
+
+        self.assertEqual(constant_part, word)
+        self.assertEqual(translated_word, 'iść')
+        self.assertEqual(part_of_speech, 'verb')
+
+    def test_word_arg_set(self):
+        word, from_lang, to_lang = 'estar', 'es', 'uk'
+        Configurations.change_last_used_languages(from_lang, to_lang)
+        self.set_input_string(f't {word}')
+
+        translation = self.translate()[0]
+        self.assertTrue(len(translation[1]))
+
+        constant_part = self.get_constant_part(translation)
+        batch = self.get_nth_translation_batch(0, translation)
+        translated_word = self.get_word_from_batch(batch)
+        part_of_speech = self.get_part_of_speech_from_batch(batch)
+
+        self.assertEqual(constant_part, word)
+        self.assertEqual(translated_word, 'бути')
+        self.assertEqual(part_of_speech, 'verb')

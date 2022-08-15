cdd28b27fd0582ec4a8879e7d4c174f421740a9b
proxpxd <piotr10tutek@poczta.onet.pl>
2022-08-15 18:19:35 +0200

    make parser more convenient by reassigning language arguments

--- src/translating/argumentParsing/intelligentArgumentParser.py
+++ src/translating/argumentParsing/intelligentArgumentParser.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable
 
 from .configurations import Configurations
 from .constants import FullModes
@@ -55,15 +55,11 @@ class IntelligentArgumentParser:
 
     def _parse_normal(self):  # TODO: add excception if no args
         self._words.append(self._get_arg_or_else(0))
-        self._from_lang = self._get_arg_else_from_config(1)
-        self._to_langs.append(self._get_arg_else_from_config(2))
-
-    def _get_arg_else_from_config(self, index: int):
-        return self._args[index] if index < len(self._args) else Configurations.get_nth_saved_language(index - 1)
+        self._parse_langs_else_get_both_from_configs(1)
 
     def _parse_multi_lang(self):
         self._words.append(self._get_arg_or_else(0))
-        self._from_lang = self._get_arg_else_from_config(1)
+        self._from_lang = self._get_arg_else_previous_index_from_config(1)
         self._to_langs = self._args[2:]
         if not self._to_langs:
             self._to_langs = self.modes.get_mode_args(FullModes.MULTI_LANG)
@@ -82,18 +78,39 @@ class IntelligentArgumentParser:
         self._words = self._args[2:]
 
     def _parse_multi_word_explicitly(self):
-        mode_index: int = self.modes.get_mode_position(FullModes.MULTI_WORD)
-        self._from_lang = self._get_arg_before_mode_else_from_config(0, mode_index)
-        self._to_langs.append(self._get_arg_before_mode_else_from_config(1, mode_index))
-        self._words = self._modesManager.get_mode_args(FullModes.MULTI_WORD)
-        if not self._words:
-            self._words = self._args[2:]
+        self._parse_langs_else_get_both_from_configs()
 
-    def _get_arg_or_else(self, i: int, otherwise: Any = None) -> Any:
-        return self._args[i] if i < len(self._args) else otherwise
+        self._words = self._args[2:]
+        self._words.extend(self._modesManager.get_mode_args(FullModes.MULTI_WORD))
 
-    def _get_arg_before_mode_else_from_config(self, arg_index: int, mode_index: int):
-        return self._args[arg_index] if arg_index < mode_index else Configurations.get_nth_saved_language(arg_index)
+    def _parse_langs_else_get_both_from_configs(self, offset=0):
+        first = self._get_arg_or_else(0 + offset)
+        if not first:
+            self._from_lang = Configurations.get_nth_saved_language(0)
+            self._to_langs.append(Configurations.get_nth_saved_language(1))
+        else:
+            self._parse_langs_else_get_both_from_configs_depending_on_second(first, offset)
+
+    def _parse_langs_else_get_both_from_configs_depending_on_second(self, first: str, offset=0):
+        second = self._get_arg_or_else(1 + offset)
+        if second:
+            self._from_lang = first
+            self._to_langs.append(second)
+        else:
+            self._from_lang = Configurations.get_nth_saved_language(0)
+            self._to_langs.append(first)
+
+    def _get_arg_else_same_from_config(self, index: int):
+        return self._get_arg_else_get(index, lambda: Configurations.get_nth_saved_language(index))
+
+    def _get_arg_else_previous_index_from_config(self, index: int):
+        return self._get_arg_else_get(index, lambda: Configurations.get_nth_saved_language(index - 1))
+
+    def _get_arg_else_get(self, index: int, func: Callable[[], str]):
+        return self._args[index] if index < len(self._args) else func()
+
+    def _get_arg_or_else(self, index: int, otherwise: Any = None) -> Any:
+        return self._args[index] if index < len(self._args) else otherwise
 
     def _remove_nones(self):
         self._to_langs = list(filter(None, self._to_langs))

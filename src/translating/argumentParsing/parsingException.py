8bcb01600f50721ecdc4bab02622f521d1c7f212
proxpxd <piotr10tutek@poczta.onet.pl>
2022-09-18 00:59:55 +0200

    getting arguments refactored and simplified
    implemented parts of the misplacement correction
    
    missing arguments correction to be implemented in one place

--- src/translating/argumentParsing/parsingException.py
+++ src/translating/argumentParsing/parsingException.py
@@ -1,4 +1,4 @@
 class ParsingException(ConnectionError):
-    def __init__(self, validation_messages: list[str], *args):
+    def __init__(self, validation_messages: list[str] = None, *args):
         super().__init__(*args)
-        self.validation_messages = validation_messages
+        self.validation_messages = validation_messages if validation_messages else []

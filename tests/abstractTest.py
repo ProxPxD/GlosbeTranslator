ec26f2e70458570c9d187c04afe5e148ccffdd7a
proxpxd <piotr10tutek@poczta.onet.pl>
2022-11-30 14:44:25 +0100

    add newer test features

--- tests/abstractTest.py
+++ tests/abstractTest.py
@@ -1,5 +1,6 @@
 import abc
 import unittest
+from typing import Iterable
 
 from src.translating.argumentParsing.configurations import Configurations, Configs
 from src.translating.argumentParsing.intelligentArgumentParser import IntelligentArgumentParser
@@ -13,6 +14,11 @@ class AbstractTest(unittest.TestCase, abc.ABC):
     translator: Translator = Translator()
     half_sep_length = 40
     currentResult = None
+
+    total = 0
+    failure = 0
+    errors = 0
+    skipped = 0
 
     @classmethod
     def print_sep_with_text(cls, text: str, sep: str = '*') -> None:
@@ -33,13 +39,47 @@ class AbstractTest(unittest.TestCase, abc.ABC):
 
     def tearDown(self) -> None:
         super().tearDown()
-        if self.currentResult is not None:
-            errors = self.currentResult.errors
-            failures = self.currentResult.failures
-            ok = not (errors or failures)
-            print('ok' if ok else 'ERROR' if errors else 'FAIL')
+        result = self.defaultTestResult()
+        self._feedErrorsToResult(result, self._outcome.errors)
+
+        is_error = any(test == self for test, text in result.errors)
+        is_failure = any(test == self for test, text in result.failures)
+        is_skipped = any(test == self for test, text in result.skipped)
+        passed = not (is_error or is_failure or is_skipped)
+
+        self.__class__.total += 1
+        if is_error:
+            self.__class__.errors += 1
+        if is_failure:
+            self.__class__.failure += 1
+        if is_skipped:
+            self.__class__.skipped += 1
+
+        print('PASS' if passed else 'ERROR' if is_error else 'FAIL' if is_failure else 'SKIP' if is_skipped else
+        'WRONG UNIT TEST OUTCOME CHECKING! Investigate (possible incompatible with a python newer than 3.10)')
+
+    def tearDownClass(cls) -> None:
+        cls.print_statistics(percentage=False)
+
+    @classmethod
+    def print_statistics(cls, failure=None, errors=None, skipped=None, total=None, *, short=False, percentage=True):
+        if failure is None:
+            failure = cls.failure
+        if errors is None:
+            errors = cls.errors
+        if skipped is None:
+            skipped = cls.skipped
+        if total is None:
+            total = cls.total
+        failed = failure + errors
+        passed = total - failed - skipped
+        if short:
+            print(f'({failure}F, {errors}E, {passed}P)/{total}')
         else:
-            print()
+            print(f'Failed: {failed} (Failures: {failure}, Errors: {errors}), Passed: {passed}, Total: {total}')
+        if percentage:
+            print(
+                f'Failed: {100 * failed / total:.1f}% (Failures: {100 * failure / total:.1f}%, Errors: {100 * errors / total:.1f})%, Passed: {100 * passed / total:.1f}%')
 
     def run(self, result: unittest.result.TestResult | None = ...) -> unittest.result.TestResult | None:
         self.currentResult = result
@@ -82,3 +122,34 @@ class AbstractTest(unittest.TestCase, abc.ABC):
 
     def get_method_name(self) -> str:
         return self.id().split('.')[-1]
+
+    @classmethod
+    def _get_test_name(cls) -> str:
+        return cls.__name__.removesuffix('Test')
+
+    def get_parameterized_methods_of_current_test(self, *method_nums) -> Iterable:
+        if not method_nums:
+            return iter([])
+
+        method_prefix = self.get_method_name() + '_'
+        child_methods = [name for name in dir(self) if name.startswith(method_prefix)]
+
+        if method_nums[0] is None:
+            method_nums = range(len(child_methods))
+        number_marks = (name.removeprefix(method_prefix).split('_')[0] for name in child_methods)
+        zero_count = len(next((num for num in number_marks if all((ch == '0' for ch in num))), ''))
+
+        method_prefixes = (f'{method_prefix}{str(i).zfill(zero_count)}' for i in method_nums)
+        methods = (getattr(self, actual_name) for expected_prefix in method_prefixes for actual_name in child_methods if
+                   actual_name.startswith(expected_prefix))
+        return methods
+
+    def run_current_test_with_params(self, *method_nums):  # TODO: support custom names
+        '''
+        Run in a method declared after the desired parametrized method and named the same as it
+        :param method_nums:
+        :return:
+        '''
+        for method in self.get_parameterized_methods_of_current_test(*method_nums):
+            method()
+
--- tests/all_tests.py
+++ tests/all_tests.py
@@ -1,9 +1,10 @@
 import unittest
 
+from tests.abstractTest import AbstractTest
+from tests.misplacedTest import MisplacedTest
 from tests.multiLangTranslationTest import MultiLangTranslationTest
 from tests.multiWordTranslationTest import MultiWordTranslationTest
 from tests.singleTranslationTest import SingleTranslationTest
-from tests.misplacedTest import MisplacedTest
 
 tests = [
     SingleTranslationTest,
@@ -14,9 +15,20 @@ tests = [
 
 
 def main():
+    failure, errors, total, skipped = 0, 0, 0, 0
     for test_class in tests:
         test = test_class()
         unittest.main(module=test, exit=False)
+
+        failure += test.failure
+        errors += test.errors
+        total += test.total
+        skipped += test.skipped
+
+    print()
+    print('#' * (2 * AbstractTest.half_sep_length))
+    print('Total test statistics:')
+    AbstractTest.print_statistics(failure, errors, skipped, total)
 
 
 if __name__ == '__main__':

611c4b4f4a799a8ad8d489e39259547f77ce5f88
proxpxd <piotr10tutek@poczta.onet.pl>
2022-12-02 13:29:23 +0100

    refactor formatting and printing to improve and match the current application style

---
+++ src/translating/translatingPrinting/formatting.py
@@ -0,0 +1,154 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from dataclasses import replace
+from typing import Iterable, Callable
+
+from more_itertools import split_before
+
+from ..constants import PageCodeMessages
+from ..parsing.parser import Record
+from ..translator import TranslationResult, TranslationTypes
+
+
+class AbstractFormatter(ABC):
+
+	@classmethod
+	@abstractmethod
+	def format(cls, to_format):
+		raise NotImplementedError
+
+	@classmethod
+	def format_many(cls, to_formats) -> Iterable:
+		return (AbstractFormatter.format(to_format) for to_format in to_formats)
+
+
+class AbstractIntoStringFormatter(ABC):
+
+	@classmethod
+	@abstractmethod
+	def format_into_string(cls, to_format, **kwargs) -> str:
+		raise NotImplementedError
+
+	@classmethod
+	def format_many_into_string(cls, to_formats: Iterable, sep: str, **kwargs) -> str:
+		formatted = map(lambda to_format: cls.format_into_string(to_formats, **kwargs), to_formats)
+		string = sep.join(formatted)
+		return string
+
+	@classmethod
+	def _add_if_exist(cls, to_add: str, brackets: str = '()', sep=' ') -> str:
+		return sep + brackets[0] + to_add + brackets[1] if to_add else ''
+
+
+class GenderFormatter(AbstractFormatter):
+
+	@classmethod
+	def format(cls, gender: str) -> str:
+		if not gender:
+			return gender
+
+		if gender == 'feminine':
+			gender = 'fem'
+		elif gender == 'masculine':
+			gender = 'masc'
+		elif gender == 'neuter':
+			gender = 'neut'
+		return gender
+
+
+class PartOfSpeechFormatter(AbstractFormatter):
+
+	@classmethod
+	def format(cls, part_of_speech: str) -> str:
+		if part_of_speech == 'adverb':
+			part_of_speech = 'adv.'
+		if part_of_speech == 'adjective':
+			part_of_speech = 'adj.'
+		return part_of_speech
+
+
+class RecordFormatter(AbstractFormatter, AbstractIntoStringFormatter):
+
+	@classmethod
+	def format(cls, record: Record):
+		record = replace(record)
+		record.gender = GenderFormatter.format(record.gender)
+		record.part_of_speech = PartOfSpeechFormatter.format(record.part_of_speech)
+		return record
+
+	@classmethod
+	def format_into_string(cls, record: Record, **kwargs) -> str:
+		string = record.translation
+		string += cls._add_if_exist(record.gender, '[]')
+		string += cls._add_if_exist(record.part_of_speech, '()')
+		return string
+
+	@classmethod
+	def format_many_into_string(cls, to_formats: Iterable, sep: str = ';', **kwargs) -> str:
+		return super().format_many_into_string(to_formats, sep, **kwargs)
+
+
+class TranslationFormatter(AbstractFormatter, AbstractIntoStringFormatter):
+
+	@classmethod
+	def format(cls, translation: TranslationResult) -> TranslationResult:
+		translation.records = RecordFormatter.format_many(translation.records)
+		return translation
+
+	@classmethod
+	def format_into_string(cls, translation: TranslationResult, **kwargs) -> str:
+		prefix_style = kwargs['prefix_style']
+		string = cls._get_prefix(translation, prefix_style)
+		string += RecordFormatter.format_many_into_string(translation.records)
+		return string
+
+	@classmethod
+	def _get_prefix(cls, translation: TranslationResult, prefix_style: TranslationTypes) -> '':
+		if prefix_style == TranslationTypes.LANG:
+			return translation.trans_args.word
+		if prefix_style == TranslationTypes.WORD:
+			return translation.trans_args.to_lang
+		if prefix_style == TranslationTypes.DOUBLE:
+			return f'{translation.trans_args.to_lang}-{translation.trans_args.word}: '
+		return ''
+
+	@classmethod  # TODO: Just in case - think of refactor
+	def format_many_into_string(cls, translations: Iterable[TranslationResult], sep: str = '\n', main_division: TranslationTypes = None,
+			prefix_style: TranslationTypes = None) -> str:
+		main_divisioned = split_before(translations, cls._get_key_function(main_division))
+		get_divider = cls._get_main_divider_getter(main_division)
+		inner_formatted = list(
+			map(lambda division: (get_divider(division), super().format_many_into_string(division, sep, prefix_style=prefix_style)), main_divisioned))
+		if len(inner_formatted) > 1:
+			pre_dash = '-' * 4
+			post_dash = '-' * 64
+			result = '\n'.join(f'{pre_dash} {divider} {post_dash}\n{inner}' for divider, inner in inner_formatted)
+		else:
+			result = inner_formatted[0][1]
+
+		return result
+
+	@classmethod
+	def _get_main_divider_getter(cls, main_division: TranslationTypes) -> Callable[[TranslationResult], str]:
+		if main_division == TranslationTypes.WORD:
+			return lambda tr: tr.trans_args.word
+		if main_division == TranslationTypes.LANG:
+			return lambda tr: tr.trans_args.to_lang
+		return lambda tr: ''
+
+	@classmethod
+	def _get_key_function(cls, main_division: TranslationTypes) -> Callable[[TranslationResult, TranslationResult], bool]:
+		if main_division == TranslationTypes.WORD:
+			return lambda tr1, tr2: tr1.trans_args.word != tr2.trans_args.word
+		if main_division == TranslationTypes.LANG:
+			return lambda tr1, tr2: tr1.trans_args.to_lang != tr2.trans_args.to_lang
+		return lambda tr1, tr2: False
+
+	# TODO: Move to printing
+	def _get_page_code_messages(self, code: int):
+		if code == 404:
+			return PageCodeMessages.PAGE_NOT_FOUND_404
+		if code == 303:
+			return PageCodeMessages.PAGE_NOT_FOUND_303
+		return PageCodeMessages.UNHANDLED_PAGE_FULL_MESSAGE.format(code)
--- src/translating/translatingPrinting/translationPrinter.py
+++ src/translating/translatingPrinting/translationPrinter.py
@@ -1,7 +1,6 @@
 import logging
 
-from .translationformatter import TranslationFormatter
-from ..argumentParsing.translatorParser import TranslatorParser
+from .formatting import TranslationFormatter
 from ..constants import LogMessages
 from ..translator import TranslationResult
 from ..web.exceptions import WrongStatusCodeException
@@ -11,63 +10,53 @@ class TranslationPrinter:
     _post_sep_length = 64
     _pre_sep_length = 4
 
-    def __init__(self):
-        self._formatter = TranslationFormatter()
-
-    def print_translations(self, translations, argument_parser: TranslatorParser):
-        translations = self._formatter.format_translations(translations)
-        if argument_parser.modes.is_single_mode_on():
-            translation = next(translations)[1]
-            self._print_single_translation_mode(translation, argument_parser)
-        else:
-            self._print_multi_translation_mode(translations, argument_parser)
+    @classmethod
+    def print_translations(cls, translations, main_division=None, prefix_style=None):
+        # if isinstance(translation.records, WrongStatusCodeException):
+        #     pass  # TODO
+        translations = TranslationFormatter.format_many(translations)
+        translation_string = TranslationFormatter.format_many_into_string(translations, prefix_style=prefix_style, main_division=main_division)
+        print(translation_string)
 
     def print_single_translation(self, translation: TranslationResult):
         if isinstance(translation.records, WrongStatusCodeException):
             pass  # TODO
-        translations = self._formatter.format_translation(translation)
+        translation = TranslationFormatter.format(translation)
+        translation_string = TranslationFormatter.format_into_string(translation)
+        print(translation.trans_args.word)
+        print(translation_string)
 
-    def _print_single_translation_mode(self, translation: list[dict, ...], argument_parser: TranslatorParser):
-        if len(translation):
-            print(argument_parser.words[0])
-            print(self._formatter.format_translation_into_string(translation))
-
-    def _print_multi_translation_mode(self, translations, argument_parser: TranslatorParser):
-        constant_elem: str = self._get_constant_translation_element(argument_parser)
-        starting_msg: str = f'{argument_parser.from_lang} -- {constant_elem}:' if constant_elem else f'{argument_parser.from_lang}:'
-
-        print(starting_msg)
-        for marker, translation in translations:
-            translation_msg = self._get_message_for_translation(translation)
-            section = self._get_section(marker)
-            variable_elem = self._get_variable_elem(marker)
-
-            if section:
-                print('-' * self._pre_sep_length, section, '-' * self._post_sep_length)
-            print(f'{variable_elem}: {translation_msg}')
+    # def _print_multi_translation_mode(self, translations, argument_parser: TranslatorParser):
+    #     constant_elem: str = self._get_constant_translation_element(argument_parser)
+    #     starting_msg: str = f'{argument_parser.from_lang} -- {constant_elem}:' if constant_elem else f'{argument_parser.from_lang}:'
+    #
+    #     print(starting_msg)
+    #     for marker, translation in translations:
+    #         translation_msg = self._get_message_for_translation(translation)
+    #         section = self._get_section(marker)
+    #         variable_elem = self._get_variable_elem(marker)
+    #
+    #         if section:
+    #             print('-' * self._pre_sep_length, section, '-' * self._post_sep_length)
+    #         print(f'{variable_elem}: {translation_msg}')
 
     def _get_message_for_translation(self, translation):
         if len(translation):
-            translation_string = self._formatter.format_translation_into_string(translation)
+            translation_string = self._formatter.format_many_into_string(translation)
         else:
             translation_string = LogMessages.NO_TRANSLATION
             logging.warning(LogMessages.NO_TRANSLATION)
         return translation_string
 
-    def _get_section(self, marker: tuple|str, sections=[]):
+    def _get_section(self, marker: tuple | str, sections=[]):
         if isinstance(marker, tuple) and marker[0] not in sections:
             sections.append(marker[0])
             return marker[0]
         return None
 
-    def _get_variable_elem(self, marker: tuple|str):
+    def _get_variable_elem(self, marker: tuple | str):
         if isinstance(marker, tuple):
             return marker[1]
         return marker
 
-    def _get_constant_translation_element(self, argument_parser: TranslatorParser):
-        if argument_parser.modes.is_multi_lang_mode_on():
-            return argument_parser.words[0]
-        elif argument_parser.modes.is_multi_word_mode_on():
-            return argument_parser.to_langs[0]
-        return ''
+    # def _get_constant_translation_element(self, argument_parser: TranslatorParser):  #     if argument_parser.modes.is_multi_lang_mode_on():  #         return argument_parser.words[0]  #     elif argument_parser.modes.is_multi_word_mode_on():  #         return argument_parser.to_langs[0]  #     return ''

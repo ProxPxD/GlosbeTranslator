f068ab78fda9e3b5dc426382f5697a1c1b6517be
proxpxd <piotr10tutek@poczta.onet.pl>
2022-12-19 22:05:18 +0100

    write taking nth language test

--- tests/configurationTest.py
+++ tests/configurationTest.py
@@ -1,3 +1,5 @@
+from parameterized import parameterized
+
 from src.glosbe.configurations import Configurations
 from tests.abstractCliTest import AbstractCliTest
 
@@ -16,10 +18,22 @@ class ConfigurationTest(AbstractCliTest):
 		super().tearDown()
 		Configurations.remove_current_configuration()
 
-	def test_take_nth_lang(self):
+	def test_take_nth_lang_from_no_langs(self):
 		Configurations.change_conf('--langs', [])
 		try:
 			Configurations.get_nth_saved_language(0)
 			Configurations.get_nth_saved_language(1)
 		except Exception:
 			self.fail()
+
+	@parameterized.expand([
+		('first', 'pl', 0),
+		('second', 'zh', 1),
+		('third', 'es', 2),
+		('forth', 'de', 3),
+		('out_of_range', None, 4),
+	])
+	def test_take_nth_lang(self, name: str, expected: str, n: int):
+		Configurations.change_conf('--langs', ['pl', 'zh', 'es', 'de'])
+		actual = Configurations.get_nth_saved_language(n)
+		self.assertEqual(expected, actual)

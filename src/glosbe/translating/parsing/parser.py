4fc3357e851ac4f6f17f684ebf3ad0bb59a79768
proxpxd <piotr10tutek@poczta.onet.pl>
2022-12-26 15:15:43 +0100

    web design update fix

--- src/glosbe/translating/parsing/parser.py
+++ src/glosbe/translating/parsing/parser.py
@@ -36,30 +36,29 @@ class Parser:
     def _parse_translation(self) -> list[Record]:
         translations = []
         soup = BeautifulSoup(self._page.text, features="html.parser")
-        for translation_element in soup.findAll('li', {'data-element': 'translation'}):
-            if translation_element.select_one('span[data-element="phrase"]'):
+        for translation_element in soup.findAll('div', {'class': 'py-1'}):
+            if translation_element.select_one('h3'):
                 translations.append(self._parse_single_translation_tag(translation_element))
         return translations
 
     def _parse_single_translation_tag(self, translation_tag: Tag) -> Record:
         translation = self._get_translation(translation_tag)
-        spans = self._get_spans(translation_tag)
-        part_of_speech = self._get_part_of_speech(spans)
-        gender = self._get_gender(spans)
-        return Record(translation, part_of_speech, gender)  # self._create_translation_part(translation, part_of_speech, gender)
+        span = self._get_spans(translation_tag)[0]
+        description = span.replace('\n', ', ')[2:-2]
+        return Record(translation, description)  # self._create_translation_part(translation, part_of_speech, gender)
 
     def _get_translation(self, translation_tag: Tag) -> str:
-        return translation_tag.select_one('span[data-element="phrase"]').text[1:-1]
+        return translation_tag.select_one('h3').text
 
     def _get_spans(self, translation_tag: Tag) -> list[str, ...]:
-        spans = translation_tag.findAll('span', {'class': 'phrase__summary__field'})
+        spans = translation_tag.findAll('span', {'class': 'text-xxs text-gray-500'})
         return [s.text for s in spans]
 
     def _get_part_of_speech(self, spans: list[str, ...]) -> str:
-        return self._get_ith_span(spans, 0)
+        return self._get_ith_span(spans, 0)[1:-1]
 
     def _get_gender(self, spans: list[str, ...]) -> str:
-        return self._get_ith_span(spans, 1)
+        return self._get_ith_span(spans, 1)[1:-1]
 
     def _get_ith_span(self, spans: list[str, ...], i: int):
         return spans[i] if len(spans) > i else ""
